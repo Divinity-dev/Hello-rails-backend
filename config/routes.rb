f07a98bbcd3d1c479c5c622a405c598080c8660b
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-06-19 17:23:39 +0100

    Add gem file to grant react front end access

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'greetings#random_greeting'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'random_greeting', to: 'greetings#random_greeting'
   # Defines the root path route ("/")
